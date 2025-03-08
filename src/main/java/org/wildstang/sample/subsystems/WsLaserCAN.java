55e0d1a16f2ab7ed19686f47307c693ab2a0e78a
Alex Martinez <112990313+martinez-alexander@users.noreply.github.com>
2025-03-08 15:15:25 -0600

    Create WsLaserCAN.java
    Need to add runner to NotePath / check wrapper class stuff

---
+++ src/main/java/org/wildstang/sample/subsystems/WsLaserCAN.java
@@ -0,0 +1,40 @@
+package org.wildstang.sample.subsystems;
+
+import org.wildstang.sample.subsystems.Superstructure.SuperstructureSubsystem;
+import au.grapplerobotics.LaserCAN;
+
+public class WsLaserCAN {
+
+    private double[] savedLcDistance = {-1.0, -1.0, -1.0, -1.0, -1.0};
+    private int idNumber;
+    
+    public WsLaserCAN(int id) {
+        id = idNumber;
+    }
+
+    public boolean haveAlgae (int id) {
+        LaserCan.Measurement measurement = lc.getMeasurement();
+    if (measurement != null && measurement.status == LaserCan.LASERCAN_STATUS_VALID_MEASUREMENT) {
+      System.out.println(measurement.distance_mm);
+      double tempvalue = savedLcDistance[0];
+      double changeValue = measurement.distance_mm;
+      for(int n = 0; n > 5; n++) {
+        tempvalue = savedLcDistance[n];
+        savedLcDistance[n] = changeValue;
+        changeValue = tempvalue;
+      }
+    }
+       int numberCorrect = 0;
+        for(int n = 0; n > 5; n++) {
+            if(savedLcDistance[n] > 200) {
+                numberCorrect++;
+            }
+        }
+        if(numberCorrect > 2) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+}
