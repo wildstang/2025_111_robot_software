16cf987b9d18a1a92b4d093ffe8fca219822b662
MichaelBauer6 <mbauer5034@stu.d214.org>
2025-04-03 19:22:28 -0500

    Done?

--- src/main/java/org/wildstang/sample/auto/Steps/SwerveTrapezoidalStep.java
+++ src/main/java/org/wildstang/sample/auto/Steps/SwerveTrapezoidalStep.java
@@ -1,89 +1,76 @@
 package org.wildstang.sample.auto.Steps;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileReader;
-import java.util.Optional;
-
 import org.wildstang.framework.auto.AutoStep;
 import org.wildstang.framework.core.Core;
-import org.wildstang.framework.subsystems.swerve.SwerveDriveTemplate;
+import org.wildstang.sample.robot.WsSubsystems;
+import org.wildstang.sample.subsystems.swerve.SwerveDrive;
+import org.wildstang.sample.subsystems.targeting.WsPose;
 
 import edu.wpi.first.math.geometry.Pose2d;
-import edu.wpi.first.math.trajectory.TrapezoidProfile;
-import edu.wpi.first.wpilibj.DriverStation;
-import edu.wpi.first.wpilibj.Filesystem;
+import edu.wpi.first.math.geometry.Transform2d;
 import edu.wpi.first.wpilibj.Timer;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
-import choreo.*;
-import choreo.trajectory.SwerveSample;
-import choreo.trajectory.Trajectory;
 
-import com.google.gson.Gson;
 
 public class SwerveTrapezoidalStep extends AutoStep {
 
-    private static final double mToIn = 39.3701;
-    private SwerveDriveTemplate m_drive;
-    private Trajectory<SwerveSample> pathtraj;
-    private SwerveSample sample;
+    private SwerveDrive swerve;
+    private WsPose pose;
+    private double turnStartTime; // Time to start turning to the end heading
 
-    // x and y field relative
+    private Pose2d startPose;
     private Pose2d fieldAutoPose;
+    private double angleToPoint;
 
-    private Timer timer;
+    private Timer turnTimer;
+    private Timer pathTimer;
+
+    private WsTrapezoidalProfile profile;
+
+    public SwerveTrapezoidalStep(SwerveDrive drive, Pose2d pose, double turnStart) {
+        this.turnStartTime = turnStart;
+        swerve = drive;
+        turnTimer = new Timer();
+        fieldAutoPose = pose;
+    }
+
+    public SwerveTrapezoidalStep(SwerveDrive drive, Pose2d pose) {
+        this(drive, pose, 0);
+    }
 
     @Override
     public void initialize() {
-        //start path
-        m_drive.setToAuto();
-        timer.start();
+        swerve.setToAuto();
+        turnTimer.start();
+        pathTimer.start();
+        profile = new WsTrapezoidalProfile(pose.estimatedPose.getTranslation().getDistance(fieldAutoPose.getTranslation()));
+        WsPose pose = (WsPose) Core.getSubsystemManager().getSubsystem(WsSubsystems.WS_POSE);
+        startPose = pose.estimatedPose;
+        angleToPoint = Math.atan2(fieldAutoPose.getY() - startPose.getY(), fieldAutoPose.getX() - startPose.getX());
     }
 
     @Override
     public void update() {
-        if (timer.get() >= pathtraj.getFinalSample(false).get().t) {
-            m_drive.setAutoValues(0,0,0,0,pathtraj.getFinalPose(false).get());
-            SmartDashboard.putNumber("auto final time", timer.get());
-            setFinished();
-        } else {
-            sample = pathtraj.sampleAt(timer.get(), false).get();
-            
-            fieldAutoPose = sample.getPose();
+        WsTrapezoidalProfile.State state = profile.calculateState(pathTimer.get());
+        double xVelocity = state.velocity() * -Math.sin(angleToPoint); // Negative so positive angle is negative velocity
+        double yVelocity = state.velocity() * Math.cos(angleToPoint);
+        double xAcceleration = state.acceleration() * -Math.sin(angleToPoint); // Negative so positive angle is negative velocity
+        double yAcceleration = state.acceleration() * Math.cos(angleToPoint); // Negative so positive angle is negative velocity
+        Pose2d position = startPose.plus(new Transform2d(startPose, fieldAutoPose).times(state.distance() / profile.getDistance()));
 
-            if (fieldAutoPose.getTranslation().getDistance(pathtraj.getFinalPose(false)
-                .get().getTranslation()) < 1.5){
-                    m_drive.setAutoValues(0.0, 0.0,0,0, pathtraj.getFinalPose(false).get());
-                    m_drive.setAutoHeading(getHeading());
-            } else {
-                m_drive.setAutoHeading(getHeading());
-                m_drive.setAutoValues(-1 * sample.vy * mToIn, sample.vx * mToIn,0,0, fieldAutoPose);
-            }
+        if (pathTimer.hasElapsed(profile.getFinalTIme())) {
+
+        }
+
+        if (turnTimer.hasElapsed(turnStartTime)) {
+            swerve.setAutoValues(xVelocity,yVelocity,xAcceleration,yAcceleration, position);
+        } else {
+            swerve.setAutoValues(xVelocity,yVelocity,xAcceleration,yAcceleration,new Pose2d(position.getTranslation(), swerve.odoAngle()));
         }
     }
 
     @Override
     public String toString() {
-        return "Swerve Auto Step";
-    }
-
-    public double getHeading(){
-        return ((-pathtraj.sampleAt(timer.get(),false).get().heading*180/Math.PI)+360)%360;
-    }
-
-    @SuppressWarnings("unchecked")
-    public Trajectory<SwerveSample> getTraj(String fileName){
-        Gson gson = new Gson();
-        var tempfile = Filesystem.getDeployDirectory();
-        var traj_dir = new File(tempfile, "choreo");
-
-        var traj_file = new File(traj_dir, fileName + ".traj");
-        try {
-            var reader = new BufferedReader(new FileReader(traj_file));
-            return  gson.fromJson(reader, Trajectory.class);
-        } catch (Exception ex) {
-            DriverStation.reportError("Choreo Trajectory get Error", ex.getStackTrace());
-        }return new Trajectory<SwerveSample>(null, null, null, null);
+        return "Trapezoidal Swerve To Point Step";
     }
 }
