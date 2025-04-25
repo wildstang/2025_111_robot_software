fe6b5cd1b2290e47f30ac2ea52b6fb13396d6bf8
MichaelBauer6 <mbauer5034@stu.d214.org>
2025-04-24 19:55:48 -0500

    Champs Logs Logs

--- src/main/java/org/wildstang/sample/robot/CANConstants.java
+++ src/main/java/org/wildstang/sample/robot/CANConstants.java
@@ -20,13 +20,13 @@ public final class CANConstants {
     public static final int GYRO = 31;
 
     //swerve constants
-    public static final int DRIVE1 = 11;
+    public static final int DRIVE1 = 11; // FL
     public static final int ANGLE1 = 12;
-    public static final int DRIVE2 = 13;
+    public static final int DRIVE2 = 13; // FR
     public static final int ANGLE2 = 14;
-    public static final int DRIVE3 = 15;
+    public static final int DRIVE3 = 15; // BL
     public static final int ANGLE3 = 16;
-    public static final int DRIVE4 = 17;
+    public static final int DRIVE4 = 17; // BR
     public static final int ANGLE4 = 18;
 
     public static final int LIFT1 = 20;
